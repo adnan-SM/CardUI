799eb5f33bc32a14aae94f37cb7c16f8dfe5ff9c
Adnan A M <adnan@screen-magic.com>
2014-01-03 18:29:01 +0530

    Updated GridView

--- src/com/example/testcardlayout/GridActivity.java
+++ src/com/example/testcardlayout/GridActivity.java
@@ -65,11 +65,7 @@ public class MainActivity extends Activity {
                                         
                                 }
 
-                                @Override
-                                public void onNothingSelected(AdapterView<?> arg0) {
-                                        // TODO Auto-generated method stub
-                                        
-                                }
+                                
                         });
         }
 
